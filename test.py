c47faad8efc16ff2671299a25c9c6fa19448d448
Andrew Chong <andrew.h.chong@gmail.com>
2016-07-27 02:45:16 +0000

    Add keyword and keywords-combination counting functions to test.py

--- test.py
+++ test.py
@@ -1,47 +1,60 @@
 # from jobs_detector import main
+import os
 from bs4 import BeautifulSoup
+import string
 # import urllib
 
 # r = urllib.urlopen('https://news.ycombinator.com/item?id=11814828').read()
 # soup = BeautifulSoup(r)
 # print type(soup)
 
-soup = BeautifulSoup(open("11814828.html"), "html.parser")
-# print soup
-parent_comment_list = []
-trtags = soup.find_all("tr", class_="athing comtr ")
-for trtag in trtags:
-    for child in trtag.descendants:
-        try:
-            if child.get('src') == "s.gif":
-                # print("found an image tag")
-                # we know this is the right image
-                if child.get('width') == "0":
-                    # print("found width == 0")
-                    parent_comment_list.append(trtag)
-        except AttributeError:
-            pass
-# tag_container = []
-# for tag in trtags:
-#     if tag.contains("img src=\"s.gif\" height=\"1\" width=\"0\""):
-#         tag_container += tag
 
-
-# print("parent_comment_list contains ", len(parent_comment_list), " values")
-# print parent_comment_list
-
-# f = open( 'output.dat', 'w' )
-# f.write(repr(parent_comment_list))
-# f.close()
-
-print parent_comment_list[0]
-print(type(parent_comment_list[0]))
+class JobPosts(object):
+    def __init__(self, page):
+        self.count = 0
+        self.page = page
+        self.job_posts = []
         
-#   continue
-# giftag = trtags.find_all("img")
-# print(giftag)
-
-
+        # Retrieves all the parent job posts, then appends to list
+        soup = BeautifulSoup(open(page), "html.parser")
+        trtags = soup.find_all("tr", class_="athing")
+        for trtag in trtags:
+            for child in trtag.descendants:
+                try:
+                    if child.get('src') == "s.gif":
+                        # print("found an image tag")
+                        # we know this is the right image
+                        if child.get('width') == "0":
+                            # print("found width == 0")
+                            self.job_posts.append(trtag)
+                except AttributeError:
+                    pass
+        
+    def __len__(self):
+        return len(self.job_posts)
+        
+    def keyword_count_in_post(self, keyword, post):
+        return string.count(str(post).upper(), keyword.upper())
+        
+    def count_keyword(self, keyword):
+        # gives count of keyword occurrences total
+        count = 0
+        for post in self.job_posts:
+            count += self.keyword_count_in_post(keyword, post)
+        return count
+    
+    def count_keywords_combination(self, keywords_string):
+        # gives count of keyword combined occurrences total
+        count = 0
+        keywords = keywords_string.upper().split("-")
+        for post in self.job_posts:
+            occurred = True
+            for keyword in keywords: #django, remote, python
+                if keyword not in str(post).upper():
+                    occurred = False
+            if occurred:
+                count += 1
+        return count
 
 
 
@@ -53,4 +66,15 @@ print(type(parent_comment_list[0]))
 # Python: 144 (16%)
 # Javascript: 118 (13%)
 # React: 133 (14%)
-# Pandas: 5 (0%)
+# Pandas: 5 (0%)            
+
+if __name__ == '__main__':
+    dir = os.path.dirname(__file__)
+    filename = os.path.join(dir, 'tests/fixtures/11814828.html')
+    posts = JobPosts(filename)
+    # print(posts.job_posts[0])
+    fn = os.path.join(os.path.dirname(__file__), 'my_file')
+
+    print(len(posts))
+    print(posts.count_keyword("python"))
+    print(posts.count_keywords_combination("python-remote-flask"))
