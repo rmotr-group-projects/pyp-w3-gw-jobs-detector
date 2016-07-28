d7aab47da4931b0b01d6eb88b903ee74ac35ff52
Andrew Chong <andrew.h.chong@gmail.com>
2016-07-28 21:12:53 +0000

    Added logic from test.py into main.py, refactored main.py to use functions instead of a class

--- jobs_detector/main.py
+++ jobs_detector/main.py
@@ -2,11 +2,59 @@ import click
 import requests
 from bs4 import BeautifulSoup
 
-from jobs_detector import settings
+import settings
 
 DEFAULT_KEYWORDS = [
-    # set some default keywords here
+     "Remote",
+    "Postgres",
+    "Python",
+    "Javascript",
+    "React",
+    "Pandas"
 ]
+
+
+
+
+def get_parent_posts(url, keywords=DEFAULT_KEYWORDS):
+    count = 0
+    job_posts = []
+    
+    # Retrieves all the parent comments, then appends to list
+    soup = BeautifulSoup(open(url), "html.parser")
+    trtags = soup.find_all("tr", class_="athing")
+    for trtag in trtags:
+        for child in trtag.descendants:
+            try:
+                if child.get('src') == "s.gif":
+                    # every HN comment/post has a spacer gif with this name
+                    if child.get('width') == "0":
+                        # HN parent comments have a s.gif with width = 0
+                        job_posts.append(trtag)
+            except AttributeError:
+                pass
+    return job_posts
+    
+def count_keyword(job_posts, keyword):
+    # gives count of keyword occurrences total
+    count = 0
+    for post in job_posts:
+        result = string.count(str(post).upper(), keyword.upper())
+        count += result
+    return count
+
+def count_keywords_combination(job_posts, keywords_string):
+    # gives count of keyword combined occurrences total
+    count = 0
+    keywords = keywords_string.upper().split("-")
+    for post in job_posts:
+        occurred = True
+        for keyword in keywords: #django, remote, python
+            if keyword not in str(post).upper():
+                occurred = False
+        if occurred:
+            count += 1
+    return count
 
 
 @click.group()
@@ -24,20 +72,24 @@ def hacker_news(post_id, keywords, combinations):
     This subcommand aims to get jobs statistics by parsing "who is hiring?"
     HackerNews posts based on given set of keywords.
     """
-    # HINT: You will probably want to use the `BeautifulSoup` tool to
-    # parse the HTML content of the website
-    soup = BeautifulSoup(html_doc, 'html.parser')
+    # setup
+    message = [] 
+    filename = "tests/fixtures/" + post_id + ".html"
+    myjobposts = get_parent_posts(filename)
+    
+    # Total job posts messge 
+    total_posts = len(myjobposts)
+    tmp_string = "Total job posts: " + str(total_posts)
+    message.append(tmp_string)
+    
+    # Stats for keywords 
+    for keyword in keywords:
+        
+    print(message)
+    
+    
+
 
 
 if __name__ == '__main__':
     jobs_detector()
-
-
-
-
-#   PLAN OF ATTACK
-#   1. Create web crawler
-#       a. Get all HTML data
-#       b. Parse HTML data into Python format (dict)
-#   2. Create func to print Python dict into human readable form
-#   3. Turn func into command line tool
