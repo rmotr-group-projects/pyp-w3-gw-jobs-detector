482e205844501b64336432c4107f6cd6492d0bcf
Nicholas Chen <njosephchen@gmail.com>
2016-07-30 23:44:34 +0000

    current draft

--- jobs_detector/main.py
+++ jobs_detector/main.py
@@ -1,32 +1,100 @@
+# -*- coding: utf-8 -*-
 import click
 import requests
 from bs4 import BeautifulSoup
+import re
 
-from jobs_detector import settings
+from .settings import BASE_URL
+from .exceptions import InvalidURLException
 
-DEFAULT_KEYWORDS = [
-    # set some default keywords here
-]
+import pdb
 
+DEFAULT_KEYWORDS = ['Remote', 'Postgres', 'Python', 'Javascript','React','Pandas']
 
 @click.group()
 def jobs_detector():
     pass
 
-
 @jobs_detector.command()
-@click.option('-i', '--post-id', type=str, required=True)
+@click.option('-i', '--post-id', type=str, required=True, help='[required]')
 @click.option('-k', '--keywords', type=str, default=','.join(DEFAULT_KEYWORDS))
 @click.option('-c', '--combinations', type=str,
               callback=lambda _, x: x.split(',') if x else x)
-def hacker_news(post_id, keywords, combinations):
+
+def hacker_news(post_id, keywords, combinations=None):
+    keywords = [word.encode('utf-8') for word in keywords.split(',')]
+    # Combine BASE_URL from setting.py with post_id (replace {} with id)
+    # Make a HTTP request from URLat(post_id))
+    r = requests.get(BASE_URL.format(post_id))
+    if r.status_code != 200:
+        raise InvalidURLException
+    
+    # BeautifulSoup above request
+    soup = BeautifulSoup(r.text, 'html.parser')
+    
+    # Get all the things (beginning of thread and comments)
+    comment_tree = soup.find_all("tr", class_= re.compile('athing')) # list of bs4.element.Tag objects; use .text to get plain-text
+    
+    click.echo(len(comment_tree))
+    # Get image tag's width to determine margin
+    job_posts = []
+    for comment in comment_tree:
+        # click.echo(len(comment_tree))
+        # click.echo(len(comment))
+        # click.echo(type(comment))
+        # click.echo(comment.text)
+        # click.echo(comment.select('img')) #[]
+        if comment.select('img'):
+            comment_width = int(comment.select('img')[0].get('width'))
+            if comment_width == 0:
+                job_posts.append(comment)
+    
     """
     This subcommand aims to get jobs statistics by parsing "who is hiring?"
     HackerNews posts based on given set of keywords.
     """
-    # HINT: You will probably want to use the `BeautifulSoup` tool to
-    # parse the HTML content of the website
-    pass
+    
+    # Finds if keyword is in job post, count++ if it is
+    
+    count_dict = {key:0 for key in keywords}
+    # click.echo(count_dict)
+    # click.echo(len(job_posts))
+    job_posts_found = 0
+    for comment in job_posts:
+        for word in keywords:
+            if word.lower() in comment.text.lower():
+                count_dict[word] += 1
+                job_posts_found += 1
+   
+    # click.echo(keywords)
+    # click.echo(count_dict)
+    # click.echo(job_posts_found)
+    
+    if combinations:
+        combination_list =  combinations.split(',')
+        combination_check_list = [item.split('-') for item in combination_list]
+        combination_dict = {key:0 for key in combination_list}
+        for comment in job_posts:
+            for combo in combination_check_list:
+                for word in combo:
+                    if word.lower() not in comment.text.lower():
+                        break
+                    elif word == keywords[-1]:
+                        combination_dict[word] += 1
+    
+    expected_list = ['Total job posts: {0}'.format(job_posts_found)]
+    expected_list.append('Keywords:') 
+    for key, val in count_dict.items():
+        expected_list.append('{0}: {1} ({2}%)'.format(key, val, int(val/float(job_posts_found)*100)))
+    
+    if combinations:
+        expected_list.append('Combinations:')
+        for key, val in combination_dict.items():
+            expected_list.append('{0}: {1} ({2}%)'.format(key, val, int(val/float(job_posts_found)*100)))
+    
+    # click.echo(len(expected_list))
+    # click.echo(expected_list) 
+    print expected_list
 
 
 if __name__ == '__main__':
