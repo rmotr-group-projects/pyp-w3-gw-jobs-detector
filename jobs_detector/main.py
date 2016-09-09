796438ad7f7ee4efcc78445f6f39dcf583a57df5
nekatak <nekatak200@yahoo.gr>
2016-09-09 15:37:16 +0000

    No tests pass yet

--- jobs_detector/main.py
+++ jobs_detector/main.py
@@ -6,8 +6,8 @@ from jobs_detector import settings
 
 DEFAULT_KEYWORDS = [
     # set some default keywords here
+'Remote', 'Postgres', 'Python', 'Javascript', 'React', 'Pandas'
 ]
-
 
 @click.group()
 def jobs_detector():
@@ -19,14 +19,44 @@ def jobs_detector():
 @click.option('-k', '--keywords', type=str, default=','.join(DEFAULT_KEYWORDS))
 @click.option('-c', '--combinations', type=str,
               callback=lambda _, x: x.split(',') if x else x)
-def hacker_news(post_id, keywords, combinations):
+
+
+def hacker_news(post_id, keywords, **combinations):
     """
     This subcommand aims to get jobs statistics by parsing "who is hiring?"
     HackerNews posts based on given set of keywords.
     """
-    # HINT: You will probably want to use the `BeautifulSoup` tool to
-    # parse the HTML content of the website
-    pass
+    print('works')
+
+    job_posts = []
+    page = requests.get(
+        "https://news.ycombinator.com/item?id=11814828")
+    soup = BeautifulSoup(page.text, 'html.parser')
+    posts = soup.find_all("tr", class_="athing")
+    for post in posts:
+        if post.find("img", width="0"):
+            job_posts.append(post)
+
+    post_matches = {}
+
+    for _post in job_posts:
+        post = _post.lower()
+        for keyword in keywords:
+            if keyword.lower() in post:
+                post_matches.setdefault(keyword, [])
+                post_matches[keyword].append(_post)
+
+    stats = []
+    total_jobs = len(job_posts)
+    stats.append('Total job posts: %s' % total_jobs)
+    stats.append('keywords:')
+    for keyword in post_matches:
+        key_stats = len( post_matches[keyword] )/total_jobs
+        str = '{}: {} ({0:.0f}%)'.format(keyword, len(post_matches[keyword]), key_stats * 100)
+        stats.append(str)
+    for message in stats:
+        print message
+
 
 
 if __name__ == '__main__':
