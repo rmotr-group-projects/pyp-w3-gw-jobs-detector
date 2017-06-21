23322d964a34c96e7f2d45a14f7c665911ad375b
Marianna <mangat.marianna@gmail.com>
2017-06-21 02:42:34 +0000

    Initial commit

--- hints/2-key-elements-in-hackernews-jobs-postings.md
+++ hints/2-key-elements-in-hackernews-jobs-postings.md
@@ -1,10 +1,19 @@
 # Hint 2
-Looking at the HTML of a hackernews posting can be a bit of a daunting task. By identifying a few key elements it can make parsing significantly easier.
+Looking at the HTML of a hackernews posting can be a bit of a daunting task. 
+By identifying a few key elements it can make parsing significantly easier.
 
 # Step 1: Identifying a comment
 
-All jobs postings are comments on a hackernews page. All contents are contained in a `<tr>` tag with a CSS class of `'.athing'`. By searching for those specific things we are able to retrieve all the comments. However this leads to an issue: This will also retrieve all the replies to postings as they are also comments. This leads us to step 2.
+All jobs postings are comments on a hackernews page. 
+All contents are contained in a `<tr>` tag with a CSS class of `'.athing'`. 
+By searching for those specific things we are able to retrieve all the comments. 
+However this leads to an issue: This will also retrieve all the replies to postings 
+as they are also comments. This leads us to step 2.
 
 # Step 2: Identifying top level comments
 
-There is a way to identify if a particular comment is a top level comment(not a reply to something) or not. Each comment has a `<td>` tag in it of class `'c00'`, and within that tag is an `<img>` tag. This image is a spacer that determines the indentation of a comment. Top level comments have `img`'s with a width of 0.
+There is a way to identify if a particular comment is 
+a top level comment(not a reply to something) or not. 
+Each comment has a `<td>` tag in it of class `'c00'`, and within that tag 
+is an `<img>` tag. This image is a spacer that determines the indentation of a comment. 
+Top level comments have `img`'s with a width of 0.
--- jobs_detector/main.py
+++ jobs_detector/main.py
@@ -6,6 +6,7 @@ from jobs_detector import settings
 
 DEFAULT_KEYWORDS = [
     # set some default keywords here
+    'Remote', 'Postgres', 'Python', 'Javascript', 'React','Pandas'
 ]
 
 
@@ -19,6 +20,7 @@ def jobs_detector():
 @click.option('-k', '--keywords', type=str, default=','.join(DEFAULT_KEYWORDS))
 @click.option('-c', '--combinations', type=str,
               callback=lambda _, __, x: x.split(',') if x else x)
+              
 def hacker_news(post_id, keywords, combinations):
     """
     This subcommand aims to get jobs statistics by parsing "who is hiring?"
@@ -26,8 +28,52 @@ def hacker_news(post_id, keywords, combinations):
     """
     # HINT: You will probably want to use the `BeautifulSoup` tool to
     # parse the HTML content of the website
-    pass
-
-
+    
+    url = settings.BASE_URL.format(post_id)
+    r = requests.get(url)
+    if r.status_code != requests.codes.ok:
+        raise Exception('Bad URL: {}'.format(url))
+    
+    html_doc = r.text
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    
+    comments = soup.find_all( 'td', class_='ind')  
+    l_jobs = []
+    
+    for icomment in comments:
+        if icomment.find('img').get('width') == "0":
+            span = icomment.parent.find('span', class_='c00')
+            l_jobs.append(span.text)
+    rs = "Total job posts: {} \nKeywords:\n".format(len(l_jobs))
+    
+    # keywords statistics
+    kw_stat={}
+    for kw in keywords.split(','):
+        j_count = 0
+        for ijob in l_jobs:
+            if kw.lower() in ijob.lower():
+                j_count += 1
+        kw_stat[kw] = j_count
+        rs += "{}: {} ({}%)\n".format(kw.title(),j_count,int((float(j_count)/len(l_jobs))* 100))
+       
+    #Combination statistics
+    if combinations:
+        combo_stat = {}
+        rs += "Combinations:\n"
+        for combo in combinations:
+            j_count = 0
+            for ijob in l_jobs:
+                is_match = True
+                for kw in combo.split('-'):
+                    if not kw.lower() in ijob.lower():
+                        is_match = False
+                if is_match:
+                    j_count += 1
+            combo_stat[combo] = j_count
+            rs += "{}: {} ({}%)\n".format(combo.title(), j_count, int(float(j_count)/len(l_jobs)* 100))
+            
+    print(rs)
+    
 if __name__ == '__main__':
     jobs_detector()
+
