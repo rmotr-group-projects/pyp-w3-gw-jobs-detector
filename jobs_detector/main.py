75c7c7a4a39ca5afcd030639ebcafb39efa00b53
Brad Schwyzer <brad.schwyzer@gmail.com>
2017-06-21 02:06:21 +0000

    Initial Commit for Brad Schwyzer and Zack Willliamson

--- jobs_detector/main.py
+++ jobs_detector/main.py
@@ -1,11 +1,19 @@
 import click
 import requests
 from bs4 import BeautifulSoup
+from collections import defaultdict
+
 
 from jobs_detector import settings
 
 DEFAULT_KEYWORDS = [
     # set some default keywords here
+    'Remote',
+    'Postgres',
+    'Python',
+    'Javascript',
+    'React',
+    'Pandas',
 ]
 
 
@@ -26,8 +34,56 @@ def hacker_news(post_id, keywords, combinations):
     """
     # HINT: You will probably want to use the `BeautifulSoup` tool to
     # parse the HTML content of the website
-    pass
+    
+    comments = []
+    kw_dict = {}
+    combo_dict = defaultdict(int)
+    
+    keylist = keywords.lower().split(',')
+    news = requests.get(settings.BASE_URL.format(post_id))
+    parsedhtml  = BeautifulSoup(news.text, 'html.parser')
+    allposts = parsedhtml.find_all('tr', class_='athing')
+    for post in allposts:
+        if post.find('img', width='0'):
+            if post.find('span', class_='c00'):
+                comments.append(post.text)
+    
+    numcomments = len(comments)
+    result = 'Total job posts: {}\n Keywords:\n'.format(numcomments)
 
+
+
+
+                
+    for comment in comments:
+        for word in keylist:
+            if word not in kw_dict:
+                kw_dict[word] = 0
+            if word in comment.lower():
+                kw_dict[word] += 1
+                
+        if combinations:
+            for combo in combinations:
+                splitcombo = combo.split('-')
+                if all([word.lower() in comment.lower() for word in splitcombo]):
+                        combo_dict[combo] += 1
+    
+    for key, value in kw_dict.items():
+        tempresult = '{}: {} ({}%)'.format(key.title(),value,int((float(value)/numcomments)*100))
+        result += tempresult + '\n'
+
+    if combinations:
+        header = 'Combinations:\n'
+        result += header
+        for key,value in combo_dict.items():
+            tempresult = '{}: {} ({}%)'.format(key.title(),value,int((float(value)/numcomments)*100))
+            result += tempresult + '\n'
+    
+
+    print(result)
+
+    
+    
 
 if __name__ == '__main__':
     jobs_detector()
